d337ef2ee9420e7e75b4212f0411643aae6ec435
reema <tharrareema@gmail.com>
2020-12-22 22:00:28 +0530

    insert guessnum basic

---
+++ guessnum.py
@@ -0,0 +1,11 @@
+import random
+random.randrange(0,10)
+def multipleclue(n):
+    r=random.randint(0,10)
+    str1=""
+    for k in range(3):
+        str1=str1 + "," + str((r+k)*n) 
+    return str1
+strresult = multipleclue(random.randrange(0,10))
+print(strresult)
+
